40a98f88ccb737f76b1e3506022089c8e1b65779
Aaron <aaronfmaartens@hotmail.com>
2020-11-18 16:35:13 +0200

    New features
    Added reflections, world env, anims ect..

--- src/app.js
+++ src/app.js
@@ -1,35 +1,36 @@
 // ******* manipulating scene objects ********
     getMicrophone();
     setupScene();
-    setupLighting();        
+    setupLighting();    
+    
     
     const floor = setupShape({            
         geometry: new THREE.PlaneGeometry(20,20,0),
-        material: new THREE.MeshStandardMaterial({color : '#ffffff', roughness: 0}),
-        pos: [0,-2,0],
-        rotation: [-171, 0, 0],
+        material: new THREE.MeshLambertMaterial( { color: 0xff6600, envMap: sphereCamera.renderTarget, combine: THREE.MixOperation, reflectivity: 0.9 } ),
+        pos: [0,-5,0],
+        rotation: [300, 0, 0],
     });        
-
+    
     const orb1 = setupShape({            
-        geometry: new THREE.SphereGeometry(1,32,32),
-        material: new THREE.MeshStandardMaterial({color : '#7ca2de', roughness: 0}),
-        pos:[-4,0,0],
-    });    
+        geometry: new THREE.SphereGeometry(1,50,50),
+        material: new THREE.MeshLambertMaterial( { color: '#9ec4ff', envMap: sphereCamera.renderTarget.texture, combine: THREE.MixOperation, reflectivity: 0.5 }),
+        pos:[-4,0,-5],
+    });        
 
     const orb2 = setupShape({
         geometry: new THREE.SphereGeometry(1,32,32),
-        material: new THREE.MeshStandardMaterial({color : '#9ec4ff', roughness: 0}),
+        material: new THREE.MeshLambertMaterial( { color: '#9ec4ff', envMap: sphereCamera.renderTarget.texture, combine: THREE.MixOperation, reflectivity: 0.5 }),
         pos:[0,0,0],
     });    
 
     const orb3 = setupShape({
         geometry: new THREE.SphereGeometry(1,32,32),
-        material: new THREE.MeshStandardMaterial({color : '#93d1fa', roughness: 0}),
-        pos:[4,0,0],
-    });    
+        material: new THREE.MeshLambertMaterial({ color: '#9ec4ff', envMap: sphereCamera.renderTarget.texture, combine: THREE.MixOperation, reflectivity: 0.5 }),
+        pos:[4,0,-5],
+    });        
 
     // *** Adding objects to scenes
-    const sceneObjects = [orb1,orb2,orb3,floor];
+    const sceneObjects = [orb1,orb2,orb3];
     addObjectsToScene(sceneObjects);
     
     const orbObjects = [orb1,orb2,orb3]  
@@ -37,15 +38,31 @@
     const defaultObjSize = 1;
     const objSelectedSize = 1.5;
    
+    let orbitRadius = 4;
+    let orbitSpeed1 = 0;    
+    let orbitSpeed2 = 0;    
+    let orbitSpeed3 = 0;    
+
     const render = () =>{
-        requestAnimationFrame(render);                                      
-        
-        orbObjects.forEach(obj => {
-            obj.mesh.rotation.x += .003;
-            obj.mesh.rotation.y += .003;            
-        })
+        sphereCamera.update(renderer,scene);
+        requestAnimationFrame(render);                                              
 
         if(audioData != undefined){
+            orbitSpeed1 += .005 + (meter.volume / 15);
+            orbitSpeed2 += .004;
+            orbitSpeed3 += .003;
+            
+            orb1.mesh.position.x = 0;                      
+            orb1.mesh.position.y = orbitRadius * Math.sin(orbitSpeed1) + 0;
+            orb1.mesh.position.z = orbitRadius * Math.sin(orbitSpeed1) + 0;
+
+            orb2.mesh.position.x = orbitRadius * Math.cos(orbitSpeed1) + 0;                      
+            orb2.mesh.position.y = 0;
+            orb2.mesh.position.z = orbitRadius * Math.sin(orbitSpeed1) + 0;
+
+            orb3.mesh.position.x = orbitRadius * Math.cos(orbitSpeed1) + 0;                      
+            orb3.mesh.position.y = orbitRadius * Math.sin(orbitSpeed1) + 0;
+            orb3.mesh.position.z = orbitRadius * Math.cos(orbitSpeed1) + 0;
 
             // Adjust lighting depending on volume of input
             directionalLight.intensity = ((meter.volume + .3) * 1.2);
@@ -59,14 +76,14 @@
                     orb.mesh.scale.y = meter.volume + defaultObjSize;
                     orb.mesh.scale.z = meter.volume + defaultObjSize;
 
-                    orb.mesh.position.y = meter.volume;
-                    orb.mesh.position.z = meter.volume; 
+                    // orb.mesh.position.y = meter.volume;
+                    // orb.mesh.position.z = meter.volume; 
                 }
             })            
         }            
 
         renderer.render(scene, camera);        
-    }      
+    }
 
-    render();
+    render();    
 // ******* manipulating scene objects end ********
--- src/scene.js
+++ src/scene.js
@@ -1,36 +1,39 @@
 // ***************** Scene ******************
-let scene, camera, renderer, composer;
+let scene, camera, renderer, composer, sphereCamera;
 
 const setupScene = () => {
     scene = new THREE.Scene();   
-    scene.background = new THREE.Color(0xdddddd);
+    //scene.background = new THREE.Color(0xdddddd);
     
-    camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,0.1,1000)            
-    camera.position.z = 10;                              
+    camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,1,5000);            
+    camera.position.set(0,0,10);                              
     
     renderer = new THREE.WebGLRenderer({antialias: true});
     //renderer.toneMapping = THREE.ReinhardToneMapping;
     //renderer.toneMappingExposure = .5;
     renderer.setSize(window.innerWidth,window.innerHeight);
-    renderer.shadowMap.enabled = true;             
+    renderer.shadowMap.enabled = true;           
     
-    let materialsArray = [];
-    let texture_bk = new THREE.TextureLoader().load('img/bluecloud_bk.jpg');
-    let texture_dn = new THREE.TextureLoader().load('img/bluecloud_dn.jpg');
-    let texture_ft = new THREE.TextureLoader().load('img/bluecloud_ft.jpg');
-    let texture_lf = new THREE.TextureLoader().load('img/bluecloud_lf.jpg');
-    let texture_rt = new THREE.TextureLoader().load('img/bluecloud_rt.jpg');
-    let texture_up = new THREE.TextureLoader().load('img/bluecloud_up.jpg');
+    let controls = new THREE.OrbitControls(camera,renderer.domElement);
+    //controls.enableZoom = false;
 
-    materialsArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
-    materialsArray.push(new THREE.MeshBasicMaterial({map: texture_dn}));
-    materialsArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
-    materialsArray.push(new THREE.MeshBasicMaterial({map: texture_lf}));
-    materialsArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
-    materialsArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
+    let urls = [        
+        'img/bluecloud_ft.jpg',
+        'img/bluecloud_bk.jpg',
+        'img/bluecloud_up.jpg',       
+        'img/bluecloud_dn.jpg',
+        'img/bluecloud_rt.jpg',
+        'img/bluecloud_lf.jpg',
+    ]
 
-    let skyBoxGeo = new THREE.BoxGeometry(10000,10000,10000);
-    
+    let loader = new THREE.CubeTextureLoader();
+    scene.background = loader.load(urls);
+
+    const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 128, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter } );
+
+    sphereCamera = new THREE.CubeCamera(1,1000,cubeRenderTarget);
+    sphereCamera.position.set(0,0,0);
+    scene.add(sphereCamera);
 
     document.body.appendChild(renderer.domElement);
 
